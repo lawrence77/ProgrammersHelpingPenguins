80d2142b5a9c7c0af3c4edb6c2b0f94c2c44ff34
Mr. Lawrence P-S <lawpeczkowski@gmail.com>
2018-03-28 17:29:25 -0400

    Fixing Linking URL

--- app/config.php
+++ app/config.php
@@ -4,7 +4,7 @@
 //path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
 define('BASE_URL', 'http://ec2-18-216-185-213.us-east-2.compute.amazonaws.com');
-define('LINK_URL', 'ProgrammersHelpingPenguins');
+define('LINK_URL', 'http://ec2-18-216-185-213.us-east-2.compute.amazonaws.com/ProgrammersHelpingPenguins/');
 
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
