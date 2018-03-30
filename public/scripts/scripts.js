b6ee7f04e4cc482f3b69340160948414a885bd64
Lawrence Peczkowski-Soto <lawpeczkowski@gmail.com>
2018-03-29 21:01:02 -0400

    Created a form to add more bomber groups.

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -10,6 +10,13 @@ $(document).ready(function(e) {
 
         clearTextOfType('#newUserForm', '.formInputBox') //clears the input boxes
     })
+    $('#newCrewButton').on('click', function(e) {
+        $(this).hide()
+
+        $('#newCrewForm')
+            .fadeIn()
+            .children('#newCrewWing').focus()
+    })
 
     $('body').find('form').find('button').on('click', function(e) {
         var buttonName = $(this).attr('name')
@@ -17,6 +24,9 @@ $(document).ready(function(e) {
             $('#newUserButton').fadeIn() //the 'Create Account' button reappears
             $('#newUserForm').hide()  //hides the new user form
             clearTextOfType('#newUserForm', '.formInputBox')  //clears any text in the form
+        } else if (buttonName == 'cancelNewCrewButton') {
+            $('#newCrewButton').fadeIn()
+            $('#newCrewForm').hide()
         }
     })
 })
