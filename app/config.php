b7866218c698c2e39b9263b56b300b36aa83f998
Bret Tomko <brett15@vt.edu>
2018-04-23 14:27:33 -0400

    Change file strucutre

---
+++ app/config.php
@@ -0,0 +1,13 @@
+<?php
+# Config file
+
+//path constants
+define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
+define('BASE_URL', 'http://localhost/cs3744/Project6/ProgrammersHelpingPenguins');
+define('LINK_URL', 'http://localhost/cs3744/Project6/ProgrammersHelpingPenguins');
+
+
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_HOST', 'localhost');
+define('DB_DATABASE', 'bomber_info'); # TODO Needs a new Database
