43500ac7cb1e06af85adc25e7f02e990d872d71f
Hanson Cress <hansoncress@gmail.com>
2018-04-26 22:20:39 -0400

    created a new branch for the graphics

--- app/view/map.tpl
+++ app/view/map.tpl
@@ -1,1 +1,4 @@
-<canvas id="beforeRaid" height="524" width="1440" ></canvas>
+<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+<canvas id="demoCanvas" width="1200" height="600" onload="drawBeforeMap()" >
+</canvas>
+<script type="text/javascript">drawBeforeMap();</script>
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -430,6 +430,92 @@ function unfollow(id, f_id) {
 }
 
 function drawBeforeMap(){
+    //Stage
+var stage = new createjs.Stage("demoCanvas");
+
+//VARIABLES
+//Drag Object Size
+dragRadius = 100;
+//Destination Size
+destHeight = 800;
+destWidth = 250;
+
+//Circle Creation
+var label = new createjs.Text("Bomber Group 405th", "14px Lato", "#fff");
+label.textAlign="center";
+label.y -= 7;
+var circle = new createjs.Shape();
+circle.graphics.setStrokeStyle(2).beginStroke("black")
+.beginFill("red").drawCircle(0,0, dragRadius);
+console.log(circle);
+
+//Drag Object Creation
+//Placed inside a container to hold both label and shape
+var dragger = new createjs.Container();
+dragger.x = dragger.y = 100;
+dragger.addChild(circle, label);
+dragger.setBounds(100, 100, dragRadius*2, dragRadius*2);
+//DragRadius * 2 because 2*r = width of the bounding box
+var label2 = new createjs.Text("Shweinfurt", "bold 20px Lato", "#000");
+label2.textAlign = "center";
+label2.x += 50;
+label2.y += 40;
+
+
+var box = new createjs.Shape();
+box.graphics.setStrokeStyle(2).beginStroke("black").rect(0, 0, destHeight, destWidth);
+var destination = new createjs.Container();
+destination.x = 350;
+destination.y = 50;
+destination.setBounds(dragRadius * 2.5 , 0, destHeight, destWidth);
+
+destination.addChild(label2, box);
+
+//DRAG FUNCTIONALITY =====================
+dragger.on("pressmove", function(evt){
+     evt.currentTarget.x = evt.stageX;
+    evt.currentTarget.y = evt.stageY;
+     stage.update(); //much smoother because it refreshes the screen every pixel movement instead of the FPS set on the Ticker
+     if(intersect(evt.currentTarget, destination)){
+       evt.currentTarget.alpha=0.2;
+       box.graphics.clear();
+       box.graphics.setStrokeStyle(3)
+       .beginStroke("#0066A4")
+       .rect(0, 0, destHeight, destWidth);
+       
+     }else{
+       evt.currentTarget.alpha=1;
+       box.graphics.clear();     box.graphics.setStrokeStyle(2).beginStroke("black").rect(0, 0, destHeight, destWidth);
+     }
+      console.log("cTarget x   " + evt.currentTarget.x + "y    " + evt.currentTarget.y + "Stage x   " +  evt.stageX + "y    " +  evt.stageY );
+});
+
+//Mouse UP and SNAP====================
+dragger.on("pressup", function(evt) {
+  if(intersect(evt.currentTarget, destination)){
+    dragger.x = destination.x + destWidth/2;
+    dragger.y = destination.y + destHeight/2;
+    dragger.alpha = 1;
+    box.graphics.clear();     
+    box.graphics.setStrokeStyle(2).beginStroke("black").rect(0, 0, destHeight, destWidth);
+    stage.update(evt);
+  }
+});
+//Tests if two objects are intersecting
+//Sees if obj1 passes through the first and last line of its
+//bounding box in the x and y sectors
+//Utilizes globalToLocal to get the x and y of obj1 in relation
+//to obj2
+//PRE: Must have bounds set for each object
+//Post: Returns true or false
+
+
+
+//Adds the object into stage
+stage.addChild(destination, dragger);
+stage.mouseMoveOutside = true;
+stage.update();
+    /*
 	var stage = new createjs.Stage("beforeRaid");
 	var crewCirlce = new createjs.Shape();
 	crewCirlce.graphics.beginFill("Red").drawCircle(0, 0, 50);
@@ -452,25 +538,40 @@ function drawBeforeMap(){
 		evt.currentTarget.y = evt.stageY;
 		stage.update();
 	});
-	
+	crewContainer.on("pressup", function(evt){
+        //evt.currentTarget.x = evt.currentTarget.x;
+        //evt.currentTarget.y = evt.currentTarget.y;
+        //alert("pressup");
+        stage.update();
+    });
 	var campaignRect = createRect("name", "cid");
 	stage.addChild(campaignRect);
 	crewContainer.on("pressup", function(evt) {
 		var temp = campaignRect;
 		console.log(temp);
 		if(temp.contains(evt.target.x ,evt.target.y)) {console.log("inside camp"); }})
-	/*redZone.on("mouseover", function(e) {
-		stage.addChild(redZoneContainer);
-		stage.update();
-	});
-	redZone.on("mouseout", function(e) {
-		stage.removeChild(redZoneContainer);
-		stage.update();
-	});*/
-	
 	stage.update();
+    */
+}
+function intersect(obj1, obj2){
+  var objBounds1 = obj1.getBounds().clone();
+  var objBounds2 = obj2.getBounds().clone();
+
+  var pt = obj1.globalToLocal(objBounds2.x, objBounds2.y);
+  
+  var h1 = -(objBounds1.height / 2 + objBounds2.height);
+  var h2 = objBounds2.width / 2;
+  var w1 = -(objBounds1.width / 2 + objBounds2.width);
+  var w2 = objBounds2.width / 2;
+ 
+  
+  if(pt.x > w2 || pt.x < w1) return false;
+  if(pt.y > h2 || pt.y < h1) return false;
+  
+  return true;
 }
 
+/*
 function createRect(name, cid)
 {
 	var rect = new createjs.Shape();
@@ -490,3 +591,4 @@ function createRect(name, cid)
 	
 	return rect;
 }
+*/
