71291edfe8ae25be30f470b292f1bd0867d11cc1
EC2 Default User <ec2-user@ip-172-31-11-27.us-east-2.compute.internal>
2018-05-01 02:12:38 +0000

    change config for hosted site

--- app/config.php
+++ app/config.php
@@ -3,8 +3,8 @@
 
 //path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL', 'http://localhost/cs3744/Project6/ProgrammersHelpingPenguins');
-define('LINK_URL', 'http://localhost/cs3744/Project6/ProgrammersHelpingPenguins');
+define('BASE_URL', 'http://ec2-18-216-185-213.us-east-2.compute.amazonaws.com/cs3744/Project6/ProgrammersHelpingPenguins');
+define('LINK_URL', 'http://ec2-18-216-185-213.us-east-2.compute.amazonaws.com/cs3744/Project6/ProgrammersHelpingPenguins');
 
 
 define('DB_USER', 'root');
